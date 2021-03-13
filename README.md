02203d53afcfa9fa3fa7452d45d787ffeaf6bc89
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-03-13 16:30:37 -0300

    iniciaizando configuraçoes para o app mobile

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# TexStyle
-A WebSite for my freind's company, the TexStyle
+# TheBarber
--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const password = 'adminPGabriel'; // a definir juntamente com o banco de dados
 const user = 'adminPGabriel';
-const dbname = 'taskListDB';
+const dbname = 'TheBarber';
 
 mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.pgpdg.mongodb.net/${dbname}?retryWrites=true&w=majority`, { useFindAndModify: false });
 
