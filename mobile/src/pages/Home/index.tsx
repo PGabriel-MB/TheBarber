728d71c20e4a0968d871749f81e2c557d51de209
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-03-23 21:43:58 -0300

    inicalizados os componentes da HomePage

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,13 +1,47 @@
 import React from "react";
-import { Container, TexLogo} from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import {
+    Container,
+    Scroller,
+    
+    HeaderArea,
+    HeaderTitle,
+    SearchButton,
 
-class Home extends React.Component {
-    render() {
-        return (
-            <Container>
-                <TexLogo>Eu sou a Home</TexLogo>
-            </Container>
-        )
-    }
+    LocationArea,
+    LocationInput,
+    LocationFinder
+} from "./styles";
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faSearch, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+const Home: React.FC = () => {
+
+    const navigation = useNavigation();
+
+    return (
+        <Container>
+            <Scroller>
+                <HeaderArea>
+                    <HeaderTitle numberOfLines={2}>
+                        Encontre o seu barbeiro favorito!
+                    </HeaderTitle>
+                    <SearchButton onPress={() => navigation.navigate('Search')}>
+                        <FontAwesomeIcon icon={ faSearch } size={26} style={{ color: "#AAAAAA" }} />
+                    </SearchButton>
+                </HeaderArea>
+
+                <LocationArea>
+                    <LocationInput />
+                    <LocationFinder>
+                        <FontAwesomeIcon icon={ faMapMarkerAlt } size={24} style={{ color: '#AAAAAA' }}  />
+                    </LocationFinder>
+                </LocationArea>
+            </Scroller>
+        </Container>
+    )
+
 }
 export default Home;
--- mobile/src/pages/Home/styles.ts
+++ mobile/src/pages/Home/styles.ts
@@ -7,8 +7,29 @@ export const Container = styled.SafeAreaView`
     align-items: center;  
 `;
 
-export const TexLogo = styled.Text`
-    color: #fff;
-    font-style: italic;
-    font-size: 40px
+export const Scroller = styled.ScrollView`
+    flex: 1;
+    padding: 20px;
 `;
+
+export const HeaderArea = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+export const HeaderTitle = styled.Text`
+    color: #ffffff;
+    font-size: 25px;
+    font-weight: bold;
+    width: 250px;
+    margin-top: 20px;
+`;
+export const SearchButton = styled.TouchableOpacity`
+    width: 26px;
+    height: 26px;
+    margin-top: 20px;
+`;
+
+export const LocationArea = styled.View``;
+export const LocationInput = styled.TextInput``;
+export const LocationFinder = styled.TouchableOpacity``;
