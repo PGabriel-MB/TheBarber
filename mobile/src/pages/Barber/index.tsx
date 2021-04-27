3a1bf56837568d9e030b34f6ab86794ee390e019
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-04-26 21:54:16 -0300

    criando a tela do barbeiro em classe

--- mobile/src/pages/Barber/index.tsx
+++ mobile/src/pages/Barber/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { UserService } from "../../api/services/UserService";
 import { Container, TexLogo} from "./styles";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 class Barber extends React.Component {
+    request = new UserService();
+    navigation = useNavigation();
+    route = useRoute();
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            userInfo: {
+                id: this.route.params.id,
+                name: this.route.params.name
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.request.getUserById()
+    }
+
     render() {
         return (
             <Container>
