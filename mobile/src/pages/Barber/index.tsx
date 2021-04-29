282e8bfd0d0780bcd507d4ed7e8185f5c9fdfc63
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-04-28 21:55:56 -0300

    pegando parametros da rota

--- mobile/src/pages/Barber/index.tsx
+++ mobile/src/pages/Barber/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { UserService } from "../../api/services/UserService";
 import { Container, TexLogo} from "./styles";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute, RouteProp } from "@react-navigation/native";
 
 interface iProps {}
 
@@ -11,28 +11,36 @@ interface iState {
     ranking: number
 }
 
+type BarberRouteProp = {
+    Barber: {
+        id: string,
+        name: string,
+        ranking: number
+    }
+}
+
 class Barber extends React.Component<iProps, iState> {
     request = new UserService();
     navigation = useNavigation();
-    route = useRoute();
+    route = useRoute<RouteProp<BarberRouteProp, 'Barber'>>();
 
     constructor(props: iProps) {
         super(props);
         this.state = {
-            id: '',
-            name: '',
-            ranking: 0
+            id: this.route.params.id,
+            name: this.route.params.name,
+            ranking: this.route.params.ranking
         }
     }
 
     componentDidMount() {
-        this.request.getUserById(this.state.id)
+        //this.request.getUserById(this.state.id)
     }
 
     render() {
         return (
             <Container>
-                <TexLogo>Eu sou Um barbeiro</TexLogo>
+                <TexLogo>Eu sou {this.state.name} </TexLogo>
             </Container>
         )
     }
