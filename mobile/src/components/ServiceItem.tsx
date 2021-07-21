0f70e90373c10ebe535f06f61a33a2a1a2f0a2bb
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-07-20 21:37:36 -0300

    criando componente para listagem dos serviços

---
+++ mobile/src/components/ServiceItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from 'styled-components/native';
+
+const Card = styled.View``;
+
+const Button = styled.TouchableOpacity``;
+
+const Price = styled.Text``;
+
+const ServiceTitle = styled.Text``;
