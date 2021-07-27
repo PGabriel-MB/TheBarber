f30700894d7b28a5e17dd0338d6f9b120ca9c92e
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-07-26 21:50:27 -0300

    melhorando a interface de serviço do barbeiro

--- mobile/src/components/ServiceItem.tsx
+++ mobile/src/components/ServiceItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import styled from 'styled-components/native';
 
@@ -5,6 +6,39 @@ const Card = styled.View``;
 
 const Button = styled.TouchableOpacity``;
 
+const ButtonText = styled.Text``;
+
+const ServiceInfo = styled.View``;
+
 const Price = styled.Text``;
 
 const ServiceTitle = styled.Text``;
+
+interface ServiceItemProps {
+    name: string,
+    price: number,
+    serviceId: string
+}
+
+
+export const ServiceItem = (props: ServiceItemProps)  => {
+    const navigation = useNavigation();
+
+    const handlePress = () => {
+        navigation.navigate
+    }
+
+    return (
+        <Card>
+            <ServiceInfo>
+                <ServiceTitle>{ props.name }</ServiceTitle>
+                <Price>{ props.price }</Price>
+            </ServiceInfo>
+            <Button>
+                <ButtonText onPress={handlePress}>
+                    Agendar
+                </ButtonText>
+            </Button>
+        </Card>
+    );
+}
