b79434658f893afc3979cf6efe09939b04b9cd7c
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-10-04 06:01:32 -0300

    definiçao de contexto user, inicio do visual tela de dados do usuario

--- mobile/src/pages/PersonalData/index.tsx
+++ mobile/src/pages/PersonalData/index.tsx
@@ -1,5 +1,9 @@
-import React, { FC } from "react";
-import { useNavigation } from '@react-navigation/native'
+import React, { FC, useState, useEffect } from "react";
+import {
+    useNavigation,
+    useRoute,
+    RouteProp
+} from '@react-navigation/native'
 
 import {
     Container,
@@ -10,10 +14,23 @@ import {
 } from "./styles";
 
 import { UserService } from "../../api/services/UserService";
+import { UserContext } from "../../contexts/UserContext";
 
+
+type PersonalDataRouteProp = {
+    User: {
+        id: string
+    }
+}
 
 const PersonalData: FC = () => {
     const request = new UserService();
+    const navigation = useNavigation();
+    const route = useRoute<RouteProp<PersonalDataRouteProp, 'User'>>();
+    const data = React.useContext(UserContext);
+
+    // definindo contexto de dados do usuario
+    // iniciando desenvolvimento de itens visuais na tela
 
     return (
         <></>
